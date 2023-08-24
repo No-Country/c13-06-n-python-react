c43b48091decf464ba1f90e2b38c88f3828d2763
javier brizuela <you@example.com>
2023-08-23 22:45:56 -0300

    connect with db

--- digimed-api/config.py
+++ digimed-api/config.py
@@ -1,6 +1,8 @@
 class DevelopmentConfig():
     DEBUG = True
-    #falta configurar la conexion a la base de datos
+    #Conexion a la base de datos
+    SQLALCHEMY_DATABASE_URI=''
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 config = {
     'development':DevelopmentConfig,
--- digimed-api/manage.py
+++ digimed-api/manage.py
@@ -1,8 +1,10 @@
 from app import create_app
 from config import config
+from flask_sqlalchemy import SQLAlchemy
 
 environment = config['development']
 app = create_app(environment)
+db = SQLAlchemy(app)
 
 if __name__=='__main__':
     app.run()
