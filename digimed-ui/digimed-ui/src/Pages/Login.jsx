ba2a30fb869da5eb15188947bde38b7389888d70
Dev7 <dev7@MacBook-Pro-de-Dev7.local>
2023-08-27 06:56:34 -0400

    footer

--- digimed-ui/digimed-ui/src/Pages/Login.jsx
+++ digimed-ui/digimed-ui/src/Pages/Login.jsx
@@ -3,7 +3,7 @@ import hero from '../assets/image_hero.png'
 
 export function Login() {
     return (  <div className="flex ">
-        <div className="">
+        <div className=" text-azul-oscuro">
        <img src={hero} alt="" className="h-[36rem] w-[50.5rem]  -ml-[5rem] object-cover"/>
      
         </div>
--- digimed-ui/digimed-ui/src/layout/Layout.jsx
+++ digimed-ui/digimed-ui/src/layout/Layout.jsx
@@ -1,4 +1,5 @@
 import Footer from "../components/Footer";
+import { Footer2 } from "../components/Footer2";
 import Navbar from "../components/Navbar";
 
 export function Layout({ children }) {
@@ -7,7 +8,7 @@ export function Layout({ children }) {
       <div className="max-w-7xl mx-auto h-screen px-20 pb-4 pt-10 flex flex-col justify-between">
         <Navbar />
         <div className="max-w-7xl">{children}</div>
-        <Footer />
+       <Footer2/>
       </div>
     </>
   );
