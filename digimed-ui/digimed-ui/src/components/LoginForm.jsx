37c4762f001f183c9c2a7f864d99b8218b41a795
Dev7 <dev7@MacBook-Pro-de-Dev7.local>
2023-08-29 09:16:47 -0400

    imput

--- digimed-ui/digimed-ui/src/components/LoginForm.jsx
+++ digimed-ui/digimed-ui/src/components/LoginForm.jsx
@@ -1,36 +1,33 @@
 export function LoginForm() {
-    return (<div className="flex flex-col ">
-        <p className="font-bold text-azul-oscuro text-3xl mb-8">Le damos la bienvenida.</p>
-        <p className="text-2xl mb-16">Si ya tiene cuenta</p>
-        <form action="" className="flex flex-col gap-6">
-            {/* <input type="text" placeholder="Email"/>
-            <input type="text" placeholder="Contraseña"/> */}
-            <div className=" mx-1 border border-zinc-500 px-3 py-4 gap-2 rounded-lg w-[25rem] flex items-center ">
-          {/* <Search className="mx-1h-5 w-5 text-zinc-500" /> */}
+  return (
+    <div className="flex flex-col ">
+      <p className="font-bold text-azul-oscuro text-3xl mb-8">
+        Le damos la bienvenida.
+      </p>
+      <p className="text-2xl mb-16">Si ya tiene cuenta</p>
+      <form action="" className="flex flex-col gap-6">
+        <div className=" mx-1 border border-zinc-500 px-3 py-4 gap-2 rounded-lg w-[25rem] flex items-center ">
           <input
             type="text"
-            className="flex-1 p-0 text-zinc-900 placeholder-zinc-600 border-0 bg-transparent"
-            placeholder="Email"
+            className="flex-1 p-0 text-zinc-900 placeholder-zinc-600  bg-transparent  focus:outline-none"
+            placeholder="Email "
           />
         </div>
-            <div className=" mx-1 border border-zinc-500 px-3 py-4 gap-2 rounded-lg w-[25rem] flex items-center ">
-          {/* <Search className="mx-1h-5 w-5 text-zinc-500" /> */}
+        <div className="  mx-1 border-2  px-3 py-4 gap-2 rounded-lg w-[25rem] flex items-center">
           <input
             type="password"
-            className="flex-1 p-0 text-zinc-900 placeholder-zinc-600 border-0 bg-transparent"
+            className="flex-1 p-0 text-zinc-900 placeholder-zinc-600  bg-transparent focus: outline-none"
             placeholder="Contraseña"
           />
         </div>
 
-            <div className=" mx-1 border border-zinc-500 px-3 py-4 bg-celeste text-white gap-2 rounded-lg w-[25rem] flex items-center  justify-center">
-          {/* <Search className="mx-1h-5 w-5 text-zinc-500" /> */}
-        
-            <button>Ingresar</button>
+
+        <div className=" mx-1 border border-zinc-500 px-3 py-4 bg-celeste text-white gap-2 rounded-lg w-[25rem] flex items-center  justify-center">
+          <button>Ingresar</button>
         </div>
+      </form>
 
-        </form>
-        
-        {/* <p>No tiene cuenta? <a href="#">Registrese</a></p> */}
-    </div>  );
+      {/* <p>No tiene cuenta? <a href="#">Registrese</a></p> */}
+    </div>
+  );
 }
-
--- digimed-ui/digimed-ui/src/components/Navbar2.jsx
+++ digimed-ui/digimed-ui/src/components/Navbar2.jsx
@@ -3,7 +3,7 @@ import logoSVG from '../assets/logo.svg'
 
 export function Navbar2() {
     return (<div className='flex items-center  bg-destacar mb-[6rem] h-[5.625rem] w-full ml-20 mr-[rem] relative justify-end'>
-            <div className="absolute -left-20">
+            <div className="absolute -left-20 -top-4">
                 <img src={logoSVG} alt="" className="" />
             </div>
         
