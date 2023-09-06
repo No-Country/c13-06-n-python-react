a54da68c05f30c891336e073d2bde8939bc0d394
javier brizuela <you@example.com>
2023-09-05 22:42:21 -0300

    add feat: models patients, doctors, medicine and prescription

--- digimed-api/app/models.py
+++ digimed-api/app/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
 
 # Clase base para crear modelos
 class Base(DeclarativeBase):
@@ -35,3 +35,47 @@ class Patient(Base):
     #Override del metodo __repr__ para mostrar los datos del paciente
     def __repr__(self):
         return f"<Patient(id='{self.id}', name='{self.name}, last_name={self.last_name}, dni={self.dni}, menber={self.member}, user_id={self.user_id}')>"
+    
+# Modelo Medico
+class Doctor(Base):
+    __tablename__ = 'doctors'
+
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(70), nullable=True)
+    last_name = Column(String(100), nullable=False)
+    registration = Column(String(100), nullable=False)
+    speciality = Column(String(), nullable=False)
+    active = Column(Boolean(), default=False)
+    user_id = Column(Integer(), ForeignKey('users.id'))
+
+#Override del metodo __repr__ para mostrar los datos del medico
+def __repr__(self):
+        return f"<Doctor(id='{self.id}', name='{self.name}, last_name={self.last_name}, registration={self.registration}, speciality={self.speciality}, active={self.active}, user_id={self.user_id}')>"
+
+# Modelo Medicamento
+class Medicine(Base):
+    __tablename__ = 'medicines'
+
+    id = Column(Integer(), primary_key=True)
+    medicine = Column(String(70), , nullable=False)
+    tradename = Column(String(70))
+    presentation = Column(String(70), nullable=False)
+    
+#Override del metodo __repr__ para mostrar los datos del medicamento
+def __repr__(self):
+        return f"<Medicine(id='{self.id}', medicine='{self.medicine}', tradename='{self.tradename}', presentation='{self.presentation}')>"
+
+# Modelo Recetas
+class Prescription(Base):
+    __tablename__ = 'prescriptions'
+
+    id = Column(Integer(), primary_key=True)
+    prescription_date = Column(DateTime(), nullable=True)
+    signature = Column(Boolean(), default=False)
+    patient_id = Column(Integer(), ForeignKey('patients.id'))
+    doctor_id = Column(Integer(), ForeignKey('doctors.id'))
+    medicine_id = Column(Integer(), ForeignKey('medicines.id'))
+
+#Override del metodo __repr__ para mostrar los datos del paciente
+def __repr__(self):
+        return f"<Prescription(id='{self.id}', prescription_date='{self.prescription_date}', signature='{self.signature}', patient_id='{self.patient_id}',doctor_id='{self.doctor_id}', medicine_id='{self.medicine_id}')>"
