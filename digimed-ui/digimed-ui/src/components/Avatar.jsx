ecb8c6632a6b2493fed509ac2236274646225e57
elvus <lvssgv@gmail.com>
2023-09-09 23:08:46 -0400

    feat: add Notification, Avatar component and return 401 status code

---
+++ digimed-ui/digimed-ui/src/components/Avatar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import avatar from '../assets/avatar.svg'
+export function Avatar(props){
+	const [showMenu, setShowMenu] = React.useState(false);
+	const handleShowMenu = () => {
+		setShowMenu(!showMenu);
+	};
+	
+	return(
+		<React.Fragment>
+			<div className="relative inline-block text-left">
+				<div onClick={handleShowMenu}>
+					{/* <button type="button" className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" id="menu-button" aria-expanded="true" aria-haspopup="true">
+						Options
+						<svg className="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+							<path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+						</svg>
+					</button> */}
+					<div className="p-4 shadow-sm bg-white w-14 h-14 rounded-full">
+						<img src={avatar} alt="" className="w-full h-full" />
+					</div>
+				</div>
+				<div className="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabIndex="-1" style={{ display: showMenu ? 'block':'none'}}>
+					<div className="py-1" role="none">
+						<div className="divide-y divide-blue-200">
+							<a href="#" className="text-gray-700 hover:text-blue-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-0">Perfil</a>
+							<a href="/" onClick={()=> localStorage.removeItem('token')} className="text-gray-700 hover:text-blue-700 block px-4 py-2 text-sm" role="menuitem" tabIndex="-1" id="menu-item-1">Salir</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	)
+}
--- digimed-ui/digimed-ui/src/components/LoginForm.jsx
+++ digimed-ui/digimed-ui/src/components/LoginForm.jsx
@@ -1,7 +1,8 @@
 // import Alert from '@mui/material/Alert';
 import Registro from '../Pages/Registro';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
+import { Notification } from './Notification';
 import axios from 'axios';
 // import { useHistory } from 'react-router-dom';
 
@@ -23,7 +24,6 @@ export function LoginForm() {
   const handlepassword = (e) => {
     setPassword(e.target.value);
   };
-
   /* console.log(email, password) */
 
   // const registrarUser = () => {
@@ -39,7 +39,7 @@ export function LoginForm() {
   const timeOutUser = () => {
     setTimeout(() => {
       setShowAlertEmail(false);
-    }, 2000);
+    }, 5000);
   };
   const enter = (e) => {
     e.preventDefault()
@@ -53,15 +53,15 @@ export function LoginForm() {
           password: password,
         })
         .then((resp) => {
-          console.log('usuario logeado')
-          // setIsAllowed(true);
-          navigate('/servicios');
+          if(resp.status === 200){
+            localStorage.setItem('token', resp.data.access_token);
+            navigate('/servicios');
+          }
           // history.push('/servicios');
           // window.location.href = '/servicios'
         })
         .catch((error) => {
           setShowAlertEmail(true);
-          timeOutUser();
         });
     }
   };
@@ -69,10 +69,6 @@ export function LoginForm() {
 
   return (
     <div className='flex flex-col '>
-      {showAlert && alert('Por favor, completa todos los campos.')}
-      {ShowAlertEmail &&
-        alert('Email no existe, por favor verifica e ingresa nuevamente')}
-
       {showLoginForm ? (
         <div>
           <p className='font-bold text-azul-oscuro text-3xl mb-8'>
@@ -94,7 +90,7 @@ export function LoginForm() {
               />
             </div>
             <div className='  mx-1 border-2  px-3 py-4 gap-2 rounded-lg w-[25rem] flex items-center'>
-              <input
+              <input 
                 onChange={handlepassword}
                 type='password'
                 name='password'
@@ -104,17 +100,12 @@ export function LoginForm() {
               />
             </div>
 
-            <div className=' mx-1 border border-zinc-500 px-3 py-4 bg-celeste text-white gap-2 rounded-lg w-[25rem] flex items-center  justify-center'>
               {/* <Search className="mx-1h-5 w-5 text-zinc-500" /> */}
 
-              <button
-                onClick={(e) => {
-                  enter(e);
-                }}
-              >
-                Ingresar
-              </button>
-            </div>
+            <button onClick={enter} className={(email && password) ? 'mx-1 border border-zinc-500 px-3 py-4 bg-celeste text-white gap-2 rounded-lg w-[25rem] flex items-center justify-center' : 'mx-1 border border-zinc-500 px-3 py-4 bg-blue-500 text-white font-bold rounded-lg w-[25rem] flex items-center justify-center opacity-50 cursor-not-allowed'} disabled={(email && password) ? false : true}>
+              Ingresar
+            </button>
+            {ShowAlertEmail ? <Notification title="Error" message="Usuario o contrasenha invalidos"/> : null}
             <div className='text-2xl mb-16 flex flex-row gap-1'>¿No tiene cuenta? <Link className='text-celeste underline' to='/Registro'> Registrese</Link></div>
           </form>
         </div>
