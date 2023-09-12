9d1bbbb646f4f589590b7e864e046cc9f44c6c64
AndresMejia <anmepe.sud@gmail.com>
2023-09-12 11:19:00 +0930

    Update Pdf

--- digimed-ui/digimed-ui/src/components/Pdf.jsx
+++ digimed-ui/digimed-ui/src/components/Pdf.jsx
@@ -1,48 +1,50 @@
-import { Page, Text, View, Document, StyleSheet } from '@react-pdf/renderer';
-
-const styles = StyleSheet.create({
-    page: {
-      // flexDirection: 'row',
-      display: 'flex',
-      // flexDirection: 'column',
-      backgroundColor: '#E4E4E4',
-      color:'green',
-      fontSize:'50px',
-      
-    },
-    section: {
-  
-      margin: 10,
-      padding: 10,
-      flexGrow: 1
-    },
-    
-  });
+import { Page, Text, View, Document } from '@react-pdf/renderer';
  
+
 
 const Pdf = ({data}) => {
   return (
-    <Document style={{}}>
-    <Page size="A4" style={styles.page}>
-      <View style={styles.section}>
+    <Document >
+    <Page size="A4" >
+      <View style={{ textAlign: 'center', marginTop: '40px', marginBottom: '20px', color: 'darkblue' }}>
+        <Text>{data.selectedProfesional}</Text>
+        <View style={{ borderBottom: '1px solid black', marginTop: '40px' }}></View>
+      </View>
+      <View style={{ textAlign: 'center', marginBottom: '20px' }}>
         <Text>Cobertura:{data.selectedCobertura}</Text>
       </View>
-      <View style={styles.section}>
+      <View style={{ textAlign: 'center', marginBottom: '20px' }} >
         <Text>Plan: {data.selectedPlan}</Text>
       </View>
-      <View style={styles.section}>
+      <View style={{ textAlign: 'center', marginBottom: '20px' }}>
         <Text>N° Afiliado: {data.afiliado}</Text>
       </View>
-      <View style={styles.section}>
+      <View style={{ textAlign: 'center', marginBottom: '20px' }}>
         <Text>Es laboral?: {data.selectedOption}</Text>
       </View>
-      <View style={styles.section}>
-        <Text>Profesional: {data.selectedProfesional}</Text>
+      <View style={{ textAlign: 'center', marginBottom: '5px' }}>
+        <Text>Formua Medica</Text>
       </View>
-      <View style={styles.section}>
+      <View style={{ textAlign: 'center' }}>
         <Text>Medicamento: {data.selectedMedicamento}</Text>
-        <View style={styles.section}>
-        <Text>Dosis: {data.selectedDosis}</Text>
+        <View style={{ textAlign: 'center', marginBottom: '100px'}}>
+        <Text >Dosis: {data.selectedDosis}</Text>
+      </View>
+
+      <View >
+      <View style={{ textAlign: 'center', marginBottom: '80px'}}>
+        <Text >Fecha </Text>
+      </View>
+      <View style={{ textAlign: 'center', marginBottom: '80px'}}>
+        <Text > {data.selectedProfesional}</Text>
+      </View>
+      </View>
+
+      <View style={{ borderTop: '1px solid black', marginTop: '20px' }}></View>
+      <View style={{ fontSize:'10px' ,paddingTop: '10px', fontStyle: 'italic' }}>
+        <Text >Una empresa dedicada al servicio de los demás</Text>
+        <Text>Todo a tus manos para mejores resultados</Text>
+        <Text style={{ color: 'darkblue', fontSize:'20px' }}>Digimed</Text>
       </View>
       </View>
     </Page>
